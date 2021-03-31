a2a5cab8942903f5a1f7b9af1b736d3928919b79
Do-Huy-1504 <ddhuy2451@gmail.com>
2021-03-31 11:33:14 +0700

    update database p1

---
+++ DB_QLKS.sql
@@ -0,0 +1,106 @@
+﻿use QLKS
+go
+
+--Tạo table
+create table THONG_TIN_KS (
+	TenKS nvarchar(20) not null primary key,
+	Dchi nvarchar(50),
+	Tphho nvarchar(20),
+	Qgia nvarchar(20),
+	Sodt nchar(11),
+	Email nchar(10),
+	Website nchar(10)
+);
+
+set dateformat dmy;
+create table KhachHang (
+	MaKH nchar(10) not null primary key,
+	TenKH nvarchar(20),
+	Gtinh nvarchar(10),
+	Cmnd nchar(10),
+	Dchi nvarchar(20),
+	Qtich nvarchar(20),
+	Sodt nchar(11),
+	Email nchar(10)
+);
+
+create table KieuPhong (
+	MaKP nchar(10) not null primary key,
+	TenKP nvarchar(20)
+);
+
+create table Phong (
+	Maphong nchar(10) not null primary key,
+	Tenphong nvarchar(20),
+	Mota nvarchar(50),
+	MaKP nchar(10),
+	MaLP nchar(10),
+	Trangthai nchar(2)
+);
+
+create table Dichvu (
+	MaDV nchar(10) not null primary key,
+	TenDV nvarchar(20),
+	GiaDV float
+);
+
+create table Hoadon (
+	MaHD nchar(10) not null primary key,
+	TenKhaiThue nvarchar(20),
+	Masothue nchar(10),
+	DchiKhaiThue nvarchar(50),
+	MaPTT nchar(10)
+);
+
+create table Phieu_Thanh_Toan (
+	MaPTT nchar(10) not null primary key,
+	SoNgay int,
+	Ngaythanhtoan date,
+	ThueVAT nchar(10),
+	TongTien float,
+	Tienphaitra float,
+	MaPDK nchar(10)
+);
+
+create table Phieu_Dang_Ky (
+	MaPDK nchar(10) not null primary key,
+	Ngayden date,
+	Ngaydi date,
+	TreEm int,
+	NguoiLon int,
+	Tratruoc nchar(10),
+	Chuthich nvarchar(100),
+	Maphong nchar(10),
+	MaDV nchar(10),
+	MaKH nchar(10)
+);
+
+set dateformat dmy;
+create table TaiKhoan_NhanVien (
+	ID_NV nchar(10) not null primary key,
+	HoTen_NV nvarchar(50),
+	ChucVu nchar(10),
+	TenTK nchar(10),
+	MatKhau nchar(10)
+);
+
+
+--Tạo khóa ngoại
+alter table Phong
+add constraint FK_Phong_KieuPhong foreign key (MaKP) references KieuPhong(MaKP)
+
+alter table Phong
+add constraint FK_Phong_LoaiPhong foreign key (MaLP) references LoaiPhong(MaLP)
+
+alter table HoaDon
+add constraint FK_HoaDon_PhieuTT foreign key (MaPTT) references Phieu_Thanh_Toan(MaPTT)
+
+alter table Phieu_Thanh_Toan
+add constraint FK_PhieuTT_PhieuDK foreign key (MaPTT) references Phieu_Dang_Ky(MaPDK);
+
+alter table Phieu_Dang_Ky
+add constraint FK_PhieuDK_DichVu foreign key (MaDV) references Dichvu(MaDV);
+alter table Phieu_Dang_Ky
+add constraint FK_PhieuDK_KhachHang foreign key (MaKH) references KhachHang(MaKH);
+alter table Phieu_Dang_Ky
+add constraint FK_PhieuDK_Phong foreign key (MaPhong) references Phong(MaPhong);
---
+++ data.sql
@@ -0,0 +1,44 @@
+﻿--Nhập dữ liệu table Tài khoản nhân viên
+INSERT INTO TaiKhoan_NhanVien
+VALUES 
+('NV001',N'Lê Thu Na',N'Tiếp tân','NV1','123456'),
+('NV002',N'Đình Trọng Ta',N'Nhân viên phục vụ phòng','NV1','123456'),
+('NV003',N'Trần Thị My Kha',N'Nhân viên quầy bar','NV2','123456'),
+('NV004',N'Lê Trọng Văn Hoàng',N'Quản lý khu giặt','NV3','123456'),
+('NV005',N'Phạm Thị Ngọc Lan',N'Quản lý đưa đón khách và spa','NV4','123456'),
+('NV006',N'Lê Trọng Kêu',N'Nhân viên lái xe','NV5','123456'),
+('NV007',N'Lê Trần Tina',N'Quản lý tổ chức tiệc cưới','NV6','123456'),
+('NV008',N'Phạm Nguyễn Gia Huy',N'Quản lý phòng tập thể hình','NV7','123456'),
+('NV009',N'Trần Thanh Bảo',N'Nhân viên bảo vệ','NV8','123456'),
+('NV010',N'La Trọng Khiêm',N'Nhân viên bảo vệ','NV9','123456'),
+('NV011',N'Quách Tuấn Du',N'Quản lý sân thể thao','NV10','123456')
+
+--Nhập dữ liệu table dịch vụ
+INSERT INTO Dichvu
+VALUES 
+('DV001',N'Spa',350000),
+('DV002',N'Thuê xe ô tô',800000),
+('DV003',N'Tổ chức tiệc cưới',10000000),
+('DV004',N'Giặt đồ',60000),
+('DV005',N'Xe đưa đón sân bay',1000000),
+('DV006',N'Phòng tập thể hình',150000),
+('DV007',N'Sân golf, tennis',150000)
+
+--Nhập dữ liệu table khách hàng
+SET DATEFORMAT DMY
+INSERT INTO KhachHang
+VALUES
+('KH001',N'Vũ Thị Thuyên','24/11/1984',N'Nữ','11142','0552684421','vtthuyen@gmail.com','123456'),
+('KH002',N'Lê Sỹ Minh','6/5/1986',N'Nam','11114','060992758','lsinh@gmail.com','123456'),
+('KH003',N'Đỗ Mạnh Tuấn','6/9/1982',N'Nam','11118','0932558624','dmtuan@gmail.com','123456'),
+('KH004',N'Nguyễn Phương Thảo','4/5/1985',N'Nữ','11115','0756883625','npthao@gmail.com','123456'),
+('KH005',N'Lê Phương Linh','3/3/1989',N'Nữ','11255','0563448552','lplinh@gmail.com','123456'),
+('KH006',N'Trần Hoàng Bảo','15/2/2000',N'Nam','20053','0693458552','thbao@gmail.com','123456'),
+('KH007',N'Võ Phước Mạnh','10/8/1999',N'Nam','12555','0574156552','vpmanh@gmail.com','123456')
+
+--Nhập dữ liệu table kiểu phòng
+insert into KieuPhong values
+('KP01','Đơn'),
+('KP01V','Đơn VIP'),
+('KP02','Đôi'),
+('KP02V','Đôi VIP')
