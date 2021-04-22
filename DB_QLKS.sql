9cd800e94e5e9b5cae9ac0c86e643b64879e81fb
Do-Huy-1504 <ddhuy2451@gmail.com>
2021-04-22 20:19:33 +0700

    Update DB_QLKS.sql
    This reverts commit d3429d8fc6433b0d55816e75b871fc721010bebd, reversing
    changes made to 00b8805f6c146961f235676e875fbe2a8ebffd78.

--- DB_QLKS.sql
+++ DB_QLKS.sql
@@ -5,14 +5,14 @@ go
 
 set dateformat dmy;
 create table KhachHang (
-	MaKH nchar(10) not null primary key,
+	ID_KH nchar(10) not null primary key,
 	TenKH nvarchar(20),
 	Gtinh nvarchar(10),
 	Cmnd nchar(10),
 	Dchi nvarchar(20),
 	Qtich nvarchar(20),
 	Sodt nchar(11),
-	Email nchar(10)
+	Email nchar(10),
 );
 
 create table KieuPhong (
@@ -26,16 +26,14 @@ create table Phong (
 	Mota nvarchar(50),
 	MaKP nchar(10),
 	MaLP nchar(10),
-	Trangthai int,
-);
+)
 
 create table Hoadon (
 	MaHD nchar(10) not null primary key,
 	TenKhaiThue nvarchar(20),
 	Masothue nchar(10),
 	DchiKhaiThue nvarchar(50),
-	MaPTT nchar(10),
-	MaNV nchar(10),
+	MaPTT nchar(10)
 );
 
 create table Phieu_Thanh_Toan (
@@ -45,7 +43,8 @@ create table Phieu_Thanh_Toan (
 	ThueVAT nchar(10),
 	TongTien float,
 	Tienphaitra float,
-	MaPDK nchar(10)
+	MaPDK nchar(10),
+	ID_NV nchar(10),
 );
 
 create table Phieu_Dang_Ky (
@@ -57,38 +56,44 @@ create table Phieu_Dang_Ky (
 	Chuthich nvarchar(100),
 	Maphong nchar(10),
 	MaDV nchar(10),
-	MaKH nchar(10)
+	ID_KH nchar(10)
 );
 
 set dateformat dmy;
 create table TaiKhoan_NhanVien (
 	ID_NV nchar(10) not null primary key,
 	TenTK nchar(10),
-	MatKhau nchar(10)
+	MatKhau nchar(10),
+	ID_Quyen nchar(10),
+);
+
+create table TaiKhoan_KhachHang (
+	ID_KH nchar(10) not null primary key,
+	TenTK nchar(20),
+	MatKhau nchar(20),
+	ID_Quyen nchar(10),
+);
+
+create table PhanQuyen (
+	ID_Quyen nchar(10) not null primary key,
+	TenQuyen nvarchar(20),
+	CoQuyen bit,
 );
 
 create table NhanVien (
 	ID_NV nchar(10) not null primary key,
-	HoTen_NV nvarchar(50),
-	ChucVu nchar(10),
+	TenNV nvarchar(20),
+	Gtinh nvarchar(10),
+	Cmnd nchar(10),
+	Dchi nvarchar(20),
+	Qtich nvarchar(20),
+	Sodt nchar(11),
+	Email nchar(10),
 );
-
-create table TK_KhachHang (
-	MaKH nchar(10) not null primary key,
-	TenTK nchar(20),
-	MatKhau nchar(20)
-);
-
 
 --Tạo khóa ngoại
 alter table Phong
 add constraint FK_Phong_KieuPhong foreign key (MaKP) references KieuPhong(MaKP);
-
-alter table HoaDon
-add constraint FK_HoaDon_NhanVien foreign key (MaNV) references NhanVien(ID_NV);
-
-alter table TK_KhachHang
-add constraint FK_KhachHang_TKKH foreign key (MaKH) references KhachHang(MaKH);
 
 alter table HoaDon
 add constraint FK_HoaDon_PhieuTT foreign key (MaPTT) references Phieu_Thanh_Toan(MaPTT);
@@ -96,10 +101,26 @@ add constraint FK_HoaDon_PhieuTT foreign key (MaPTT) references Phieu_Thanh_Toan
 alter table Phieu_Thanh_Toan
 add constraint FK_PhieuTT_PhieuDK foreign key (MaPTT) references Phieu_Dang_Ky(MaPDK);
 
-alter table Phieu_Dang_Ky
-add constraint FK_PhieuDK_KhachHang foreign key (MaKH) references KhachHang(MaKH);
-alter table Phieu_Dang_Ky
-add constraint FK_PhieuDK_Phong foreign key (MaPhong) references Phong(MaPhong);
+alter table TaiKhoan_KhachHang
+add constraint FK_TKKH_KhachHang foreign key (ID_KH) references KhachHang(ID_KH);
 
 alter table TaiKhoan_NhanVien
 add constraint FK_TKNV_NhanVien foreign key (ID_NV) references NhanVien(ID_NV);
+
+alter table Phieu_Dang_Ky
+add constraint FK_PhieuDK_DichVu foreign key (MaDV) references Dichvu(MaDV);
+
+alter table Phieu_Dang_Ky
+add constraint FK_PhieuDK_KhachHang foreign key (ID_KH) references KhachHang(ID_KH);
+
+alter table Phieu_Dang_Ky
+add constraint FK_PhieuDK_Phong foreign key (MaPhong) references Phong(MaPhong);
+
+alter table TaiKhoan_KhachHang
+add constraint FK_TKKH_PhanQuyen foreign key (ID_Quyen) references PhanQuyen(ID_Quyen);
+
+alter table TaiKhoan_NhanVien
+add constraint FK_TKNV_PhanQuyen foreign key (ID_Quyen) references PhanQuyen(ID_Quyen);
+
+alter table Phieu_Thanh_Toan
+add constraint FK_PTT_NhanVien foreign key (ID_NV) references NhanVien(ID_NV);
