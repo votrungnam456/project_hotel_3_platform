d4722ed144e94a89324fa12c1d833b7b68ae9bb3
HorikitanFutaba <votrungnam2013@gmail.com>
2021-04-24 19:38:10 +0700

    update database

--- DB_QLKS.sql
+++ DB_QLKS.sql
@@ -1,114 +1,112 @@
-﻿use QLKS
+﻿create database QLKS
+use QLKS
 go
 
 --Tạo table
 
 set dateformat dmy;
 create table KhachHang (
-	ID_KH nchar(10) not null primary key,
-	TenKH nvarchar(20),
+	ID_KH char(20) not null primary key,
+	TenKH nvarchar(50),
 	Gtinh nvarchar(10),
-	Cmnd nchar(10),
-	Dchi nvarchar(20),
-	Qtich nvarchar(20),
-	Sodt nchar(11),
-	Email nchar(10),
+	Cmnd char(10),
+	Dchi nvarchar(50),
+	Qtich nvarchar(50),
+	Sodt char(11),
+	Email char(50),
+	MatKhau nchar(10)
 );
 
 create table KieuPhong (
-	MaKP nchar(10) not null primary key,
-	TenKP nvarchar(20)
+	MaKP char(20) not null primary key,
+	TenKP nvarchar(50)
 );
 
 create table Phong (
-	Maphong nchar(10) not null primary key,
-	Tenphong nvarchar(20),
+	Maphong char(20) not null primary key,
+	Tenphong nvarchar(50),
 	Mota nvarchar(50),
-	MaKP nchar(10),
-	MaLP nchar(10),
+	MaKP char(20),
+	Gia int,
+	TinhTrang int
 )
 
-create table Hoadon (
-	MaHD nchar(10) not null primary key,
-	TenKhaiThue nvarchar(20),
-	Masothue nchar(10),
-	DchiKhaiThue nvarchar(50),
-	MaPTT nchar(10)
+create table HoaDonDatPhong (
+	MaHDDP char(20) not null primary key,
+	MaPDK char(20),
+	NgayLap date
 );
+
+
+
+create table HoaDonDichVu(
+	ID_HDDV char(20) not null primary key,
+	ID_DV char(20),
+	ID_KH char(20),
+	NgayLap date
+)
+
+
+create table DichVu(
+	ID_DV char(20) not null primary key,
+	TenDV nvarchar(50),
+	Gia int
+)
 
 create table Phieu_Thanh_Toan (
-	MaPTT nchar(10) not null primary key,
-	SoNgay int,
-	Ngaythanhtoan date,
-	ThueVAT nchar(10),
-	TongTien float,
-	Tienphaitra float,
-	MaPDK nchar(10),
-	ID_NV nchar(10),
+	MaPTT char(20) not null primary key,
+	ID_KH char(20),
+	TienThuePhong int,
+	TienDichVu int,
+	TongTien int,
+	NgayThanhToan date,
+	ID_NV char(20)
 );
+
 
 create table Phieu_Dang_Ky (
-	MaPDK nchar(10) not null primary key,
+	MaPDK char(20) not null primary key,
 	Ngayden date,
 	Ngaydi date,
-	SoNguoi int,
-	Tratruoc nchar(10),
 	Chuthich nvarchar(100),
-	Maphong nchar(10),
-	MaDV nchar(10),
-	ID_KH nchar(10)
-);
-
-set dateformat dmy;
-create table TaiKhoan_NhanVien (
-	ID_NV nchar(10) not null primary key,
-	TenTK nchar(10),
-	MatKhau nchar(10),
-	ID_Quyen nchar(10),
-);
-
-create table TaiKhoan_KhachHang (
-	ID_KH nchar(10) not null primary key,
-	TenTK nchar(20),
-	MatKhau nchar(20),
-	ID_Quyen nchar(10),
+	Maphong char(20),
+	ID_KH char(20)
 );
 
 create table PhanQuyen (
-	ID_Quyen nchar(10) not null primary key,
+	ID_Quyen char(20) not null primary key,
 	TenQuyen nvarchar(20),
 	CoQuyen bit,
 );
-
-create table NhanVien (
-	ID_NV nchar(10) not null primary key,
-	TenNV nvarchar(20),
-	Gtinh nvarchar(10),
-	Cmnd nchar(10),
-	Dchi nvarchar(20),
-	Qtich nvarchar(20),
-	Sodt nchar(11),
-	Email nchar(10),
+create table TaiKhoan_NhanVien (
+	ID_NV char(20) not null primary key,
+	Email char(20),
+	MatKhau char(20),
+	ID_Quyen char(20),
 );
 
+
+
+create table NhanVien (
+	ID_NV char(20) not null primary key,
+	TenNV nvarchar(50),
+	Gtinh nvarchar(10),
+	Cmnd char(10),
+	Dchi nvarchar(50),
+	Sodt nchar(11),
+);
+
+
+
 --Tạo khóa ngoại
-alter table Phong
-add constraint FK_Phong_KieuPhong foreign key (MaKP) references KieuPhong(MaKP);
 
-alter table HoaDon
-add constraint FK_HoaDon_PhieuTT foreign key (MaPTT) references Phieu_Thanh_Toan(MaPTT);
 
-alter table Phieu_Thanh_Toan
-add constraint FK_PhieuTT_PhieuDK foreign key (MaPTT) references Phieu_Dang_Ky(MaPDK);
-
-alter table TaiKhoan_KhachHang
-add constraint FK_TKKH_KhachHang foreign key (ID_KH) references KhachHang(ID_KH);
 
 alter table TaiKhoan_NhanVien
 add constraint FK_TKNV_NhanVien foreign key (ID_NV) references NhanVien(ID_NV);
+alter table TaiKhoan_NhanVien
+add constraint FK_TKNV_PhanQuyen foreign key (ID_Quyen) references PhanQuyen(ID_Quyen);
 
-alter table Phieu_Dang_Ky
-add constraint FK_PhieuDK_DichVu foreign key (MaDV) references Dichvu(MaDV);
 
 alter table Phieu_Dang_Ky
 add constraint FK_PhieuDK_KhachHang foreign key (ID_KH) references KhachHang(ID_KH);
@@ -116,11 +114,18 @@ add constraint FK_PhieuDK_KhachHang foreign key (ID_KH) references KhachHang(ID_
 alter table Phieu_Dang_Ky
 add constraint FK_PhieuDK_Phong foreign key (MaPhong) references Phong(MaPhong);
 
-alter table TaiKhoan_KhachHang
-add constraint FK_TKKH_PhanQuyen foreign key (ID_Quyen) references PhanQuyen(ID_Quyen);
 
-alter table TaiKhoan_NhanVien
-add constraint FK_TKNV_PhanQuyen foreign key (ID_Quyen) references PhanQuyen(ID_Quyen);
+alter table HoaDonDichVu
+add constraint FK_HoaDonDichVu_DichVu foreign key (ID_DV) references DichVu(ID_DV);
+alter table HoaDonDichVu
+add constraint FK_HoaDonDichVu_KhachHang foreign key (ID_KH) references KhachHang(ID_KH);
 
+alter table Phong
+add constraint FK_Phong_KieuPhong foreign key (MaKP) references KieuPhong(MaKP);
+alter table HoaDonDatPhong
+add constraint FK_HoaDonDatPhong_PhieuDK foreign key (MaPDK) references Phieu_Dang_Ky(MaPDK);
+
+alter table Phieu_Thanh_Toan
+add constraint FK_PhieuTT_KhachHang foreign key (ID_KH) references KhachHang(ID_KH);
 alter table Phieu_Thanh_Toan
 add constraint FK_PTT_NhanVien foreign key (ID_NV) references NhanVien(ID_NV);
